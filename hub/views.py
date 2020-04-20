b3b627b4cd88f4ca077a4ffe6e1fdeb0d6229281
ben <benthefreelancer@gmail.com>
2020-04-19 19:34:38 -0700

    Updated Views

--- hub/views.py
+++ hub/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'hub/DIYHUB.html')
+
+def login(request):
+    return render(request, 'hub/login.html')
+
+def register(request):
+    return render(request, 'hub/register.html')
+
+def profile(request):
+    return render(request, 'hub/profile.html')
+
+def createProject(request):
+    return render(request, 'hub/createProject.html')
+
+def project(request):
+    return render(request, 'hub/project.html')
