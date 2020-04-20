00452560308f8dd9a357f05fdc518883ce9c45df
ben <benthefreelancer@gmail.com>
2020-04-19 20:48:19 -0700

    added models for DB

--- hub/models.py
+++ hub/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Project(models.Model):
     name = models.TextField()
     desc = models.TextField()
+    #partsNeeded = ArrayField(models.TextField(), blank=True)
+    difficulty = models.IntegerField(default=0)
+    #url = models.TextField()
 
 class User(models.Model):
     username = models.TextField()
     password = models.TextField()
     email = models.TextField()
-    phone = models.IntegerField
+    #phone = models.IntegerField()
+    #profilePicture = models.TextField()
+    #bio = models.TextField()
+    #parts = ArrayField(models.TextField(), blank=True)
+    #projects = ArrayField(models.TextField(), blank=True)
+    #savedProjects = ArrayField(models.TextField(), blank=True)
+
+class Parts(models.Model):
+    name = models.TextField()
+    url = models.TextField()
--- hub/views.py
+++ hub/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'hub/DIYHUB.html')
+
 
 def login(request):
     return render(request, 'hub/login.html')
 
+
 def register(request):
     return render(request, 'hub/register.html')
+
 
 def profile(request):
     return render(request, 'hub/profile.html')
 
+
 def createProject(request):
     return render(request, 'hub/createProject.html')
+
 
 def project(request):
     return render(request, 'hub/project.html')
