1c37b020490272ce0288a84f77f7204960ebbe25
ben <benthefreelancer@gmail.com>
2020-04-19 17:16:00 -0700

    added PostgreSQL

--- hub/models.py
+++ hub/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+
+class User(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+    email = models.TextField()
+    phone = models.IntegerField
