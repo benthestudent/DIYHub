4ac49ce31e2f98e49f25c540cf80bdf0ed410e18
benthestudent <bensstudentemail@gmail.com>
2020-06-27 06:07:40 +0000

    server settings

--- DIYHub/wsgi.py
+++ DIYHub/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for DIYHub project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DIYHub.settings')
+sys.path.append('/opt/bitnami/apps/django/django_projects/DIYHub')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/DIYHub/egg_cache")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DIYHub.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
