514674ab7cd305931b120490cbe7a4c186c06363
Elijah <elijah.kane.1972@gmail.com>
2020-07-13 01:11:04 -0700

    Optimized Images

--- image_compressor.py
+++ image_compressor.py
@@ -1,14 +1,3 @@
-from PIL import Image
-
-IMG = "hub/static/img/ben.jpg"
-SCALE = 1.5
-
-foo = Image.open(IMG)
-
-WIDTH_SCALED = foo.width / int(SCALE)
-HEIGHT_SCALED = foo.height / int(SCALE)
-
-foo = foo.resize((int(WIDTH_SCALED), int(HEIGHT_SCALED)), Image.ANTIALIAS)
-foo.save("hub/static/img/optimized/ben_scaled.jpg", quality=95)
-
-foo.save("hub/static/img/optimized/ben_optimized.jpg", optimize=True, quality=95)
+import os
+IMG_DIRECTORY = './hub/static/img/optimized'
+os.system('optimize-images ' + str(IMG_DIRECTORY))
